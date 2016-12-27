81ffab0d3a399c5a88a12d0ad7d3c19bbb973532
MarcManiez <marc.maniez@gmail.com[A>
2016-12-27 12:55:54 -0800

    Reset Search state and clear formon submit

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -17,13 +17,17 @@ class Search extends React.Component {
     this.props.fetch()
   }
   onKeyPress(event) {
-    if (event.which === 13) {
+    if (event.which === 13 && this.state.location !== null && this.state.activity !== null) {
       this.props.fetch(this.props.setData, {location: this.state.location, activity: this.state.activity});
+      this.refs.search.reset();
+      debugger;
+      $('.search-bar').first().focus();
+      this.setState({location: null, activity: null});
     }
   }
   render() {
     return (
-      <form onKeyPress={this.onKeyPress.bind(this)} className="row">
+      <form onKeyPress={this.onKeyPress.bind(this)} className="row" ref="search">
         <div className="col-md-6 col-xs-6">
           <div className="input-group">
             <SearchBar updateSearchTerms={this.updateSearchTerms.bind(this)} searchType={'location'}/>
@@ -34,7 +38,6 @@ class Search extends React.Component {
             <SearchBar updateSearchTerms={this.updateSearchTerms.bind(this)} searchType={'activity'}/>
           </div>
         </div>
-        
       </form>
     );
   }
