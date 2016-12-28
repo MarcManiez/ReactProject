2f7c2ff30a192ff40aa98775325e1f4574996d9b
MarcManiez <marc.maniez@gmail.com[A>
2016-12-27 19:40:52 -0800

    Minor cosmetic adjustments to Weather module

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -9,17 +9,19 @@ class Weather extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.currentActivity !== null) {
       $.getJSON(`http://api.openweathermap.org/data/2.5/weather?lat=${nextProps.currentActivity.lat}&lon=${nextProps.currentActivity.lon}&units=imperial&appid=6aacd398ae4803d7a0bf312facdbfaef`, (json) => {
-        console.log(json);
-        // this.setState({
-        //   lat: jason
-        // });
+        this.setState({
+          weather: json.weather[0].description,
+          temperature: json.main.temp.toFixed(1)
+        });
       });
     }
   }
   render() {
     return (
+      !this.props.currentActivity ? <div className='weather'>Area loading...</div> :
       <div className='weather'>
         <h3>Weather</h3>
+        <p>{this.state.weather} and {this.state.temperature}°F</p>
       </div>
     );
   }
