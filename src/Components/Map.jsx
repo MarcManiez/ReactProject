681c641f8bfa7aecba93e0f04fd2f08ee379d16e
MarcManiez <marc.maniez@gmail.com[A>
2016-12-28 13:42:47 -0800

    Integrate basic version of Google maps API

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,5 +1,29 @@
-const Map = (props) => (
-  <div className='map'>
-    {getMap(props.currentActivity)}
-  </div>
-);
+const Map = (props) => {
+  // if (props.currentActivity) {
+  //   const initMap = getMap(props.currentActivity.lat, props.currentActivity.lon);
+  // }
+  return (
+    !props.currentActivity ? <div className='map'>Map loading...</div> :
+    <div className="map">
+      <iframe src={`//www.google.com/maps/embed/v1/place?q=Harrods,Brompton%20Rd,%20UK&zoom=17&key=${credentials.mapsKey}`}></iframe>
+    </div>
+  );
+};
+
+    // <div className="map">
+    //   <div id="map"></div>
+    //   <script>
+    //     function initMap() {
+    //       var uluru = {lat: -25.363, lng: 131.044};
+    //       var map = new google.maps.Map(document.getElementById('map'), {
+    //         zoom: 4,
+    //         center: uluru
+    //       });
+    //       var marker = new google.maps.Marker({
+    //         position: uluru,
+    //         map: map
+    //       });
+    //     }
+    //   </script>
+    //   <script src={`https://maps.googleapis.com/api/js?key=${credentials.mapsKey}&callback=initMap`} async defer></script>
+    // </div>
--- src/lib/getMap.js
+++ src/lib/getMap.js
@@ -1,3 +1,9 @@
-const getMap = function() {
-
+const getMap = function(lat, lon) {
+  return function() {
+    console.log('Google maps api is live!');
+    const map = new google.maps.Map($('.map'), {
+      center: {lat: lat, lng: lon},
+      zoom: 8
+    });
+  }
 };
