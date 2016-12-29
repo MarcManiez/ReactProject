aaab08bfb4a4bb8c5d35cfe5ef7294fd586f2707
MarcManiez <marc.maniez@gmail.com[A>
2016-12-28 17:47:55 -0800

    Refactor MainView to stateful class component

--- src/Components/MainView.jsx
+++ src/Components/MainView.jsx
@@ -1,13 +1,21 @@
-const MainView = ({currentActivity}) => (
-  <div>
-    <div className='row main-view'>
-      <div className="col-md-8">
-        <ActivityDisplay currentActivity={currentActivity}/>
+class MainView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div className='row main-view'>
+          <div className="col-md-8">
+            <ActivityDisplay currentActivity={this.props.currentActivity}/>
+          </div>
+          <div className="col-md-4 map-weather-col">
+            <Map currentActivity={this.props.currentActivity}/>
+            <Weather currentActivity={this.props.currentActivity}/>
+          </div>
+        </div>
       </div>
-      <div className="col-md-4 map-weather-col">
-        <Map currentActivity={currentActivity}/>
-        <Weather currentActivity={currentActivity}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
