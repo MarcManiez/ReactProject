a0b6d650eb7282c6044a6ac09ccacc37d38be53c
MarcManiez <marc.maniez@gmail.com[A>
2016-12-28 17:58:38 -0800

    Refactor Weather to stateless functional component

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,30 +1,7 @@
-class Weather extends React.Component { // try to refactor as stateless component
-  constructor(props) {
-    super(props);
-    this.state = {
-      weather: null,
-      temperature: null
-    }
-  }
-  componentWillReceiveProps(nextProps) {
-    if (!nextProps.currentActivity) return
-    $.getJSON(`http://api.openweathermap.org/data/2.5/weather?lat=${nextProps.currentActivity.lat}&lon=${nextProps.currentActivity.lon}&units=imperial&appid=6aacd398ae4803d7a0bf312facdbfaef`, (json) => {
-      this.setState({
-        weather: json.weather[0].description,
-        temperature: json.main.temp.toFixed(1)
-      });
-    }); // refactor this into a different file
-    // if (nextProps.currentActivity) {
-    //   getImage(nextProps.currentActivity.name);
-    // }
-  }
-  render() {
-    return (
-      !this.props.weatherInfo ? <div className='weather'>Weather loading...</div> :
-      <div className='weather'>
-        <h3>Weather</h3>
-        <p>{this.props.weatherInfo.weather} and {this.props.weatherInfo.temperature}°F</p>
-      </div>
-    );
-  }
-}
+const Weather = (props) => (
+  !props.weatherInfo ? <div className='weather'>Weather loading...</div> :
+  <div className='weather'>
+    <h3>Weather</h3>
+    <p>{props.weatherInfo.weather} and {props.weatherInfo.temperature}°F</p>
+  </div>
+);
